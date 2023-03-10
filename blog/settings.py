08ad4b9cbd5411e838d790bf14eec9ad9b74f830
Dayan1533 <dayanramos5537@gmail.com>
2023-03-10 17:21:25 -0300

    Style: added templates

--- blog/settings.py
+++ blog/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILE_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILE_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
